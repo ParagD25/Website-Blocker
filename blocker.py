2013e32b473634755e9ac990847c23af9d0a346c
Parag <paragdurafe@outlook.com>
2021-08-24 23:58:06 +0530

    setting blocking hours

--- blocker.py
+++ blocker.py
@@ -4,3 +4,7 @@ from datetime import datetime as dt
 path_host=r"C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 blocking_website_list=["twitter.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,10)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,18):
+        print('Working Time')
