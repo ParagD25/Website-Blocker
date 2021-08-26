00d136f40f485df38ffeb2a6670da5a43c5ae4a9
Parag <paragdurafe@outlook.com>
2021-08-26 19:54:53 +0530

    Websites added

--- blocker.py
+++ blocker.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime as dt
 
-path_host=r"C:\Windows\System32\drivers\etc\hosts"
+# path_host=r"C:\Windows\System32\drivers\etc\hosts"
 temp_path='hosts'
 redirect="127.0.0.1"
 blocking_website_list=["twitter.com"]
@@ -13,4 +13,15 @@ while True:
             for website in blocking_website_list:
                 if website in content:
                     pass
+                else:
+                    file.write(redirect+" "+website+"\n")
+    else:
+        with open(temp_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in blocking_website_list):
+                    file.write(line)
+            file.truncate()
+        
         
